8d580084ec5b127c255d5bc4250134e85bbc2853
chrisXdevloper <150897697+chrisXdevloper@users.noreply.github.com>
2025-08-17 23:56:52 +0530

    Update hosting.py

--- project/hosting/hosting.py
+++ project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="project/deployment",     # the local folder containing your files
-    repo_id="zezkcy/zezkcy/Tour-Package",          # the target repo
+    repo_id="zezkcy/Tour-Package",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside 
 )
